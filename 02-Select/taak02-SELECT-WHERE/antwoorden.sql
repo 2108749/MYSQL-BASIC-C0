5888ca88bfb18b55b90c79e09dc6395a86b05a3b
Diego Blom <2108749@talnet.nl>
2020-12-10 13:23:08 +0100

    antwoorden eindopdracht

--- 02-Select/taak02-SELECT-WHERE/antwoorden.sql
+++ 02-Select/taak02-SELECT-WHERE/antwoorden.sql
@@ -1,8 +1,26 @@
 -- Opdracht 1 
+127.0.0.1/mod-mysql-basic-worldhappiness/jaar2015/		http://localhost/phpmyadmin/tbl_sql.php?db=+mod-mysql-basic-worldhappiness&table=jaar2015
+ Weergave van records 0 -  0 (1 totaal, Query duurde 0,0023 seconden.)
+
+SELECT * FROM jaar2015 WHERE RANK=7
+
+
+
+Netherlands	Western Europe	7	7378	
+
 
 -- Opdracht 2 
+127.0.0.1/mod-mysql-basic-worldhappiness/jaar2016/		http://localhost/phpmyadmin/tbl_sql.php?db=+mod-mysql-basic-worldhappiness&table=jaar2016
+ Weergave van records 0 -  0 (1 totaal, Query duurde 0,0124 seconden.)
+
+SELECT `rank` FROM `jaar2016` WHERE country="Netherlands"
+
+
+
+7	
 
 -- Opdracht 3
+
 
 -- Opdracht 4
 
